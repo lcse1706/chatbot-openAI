e4884c0e8dce40313713a529081f97aaa2c7910d
lukczar1706 <lcse.2001@gmail.com>
2024-09-04 15:42:39 +0200

    build: simple front-end chat implemented

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const port = 3001;
 

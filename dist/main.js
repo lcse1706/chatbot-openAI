503484df37c0bb54c5965c58fb518fa765bccda1
lukczar1706 <lcse.2001@gmail.com>
2024-09-05 14:34:01 +0200

    feat: global prefix - api

--- dist/main.js
+++ dist/main.js
@@ -6,11 +6,13 @@ const common_1 = require("@nestjs/common");
 const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
     app.enableCors();
     app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
     const port = 3001;
     await app.listen(port);
-    common_1.Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+    common_1.Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map

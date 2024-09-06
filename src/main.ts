a48c0b7e8446c22e66b9819d8795564de58ae83f
lukczar1706 <lcse.2001@gmail.com>
2024-09-06 14:53:30 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://jobboard-pi.vercel.app', 'http://localhost:3002'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const port = process.env.PORT || 3001; //
 
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: ${process.env.VERCEL_URL || `http://localhost:${port}`}/${globalPrefix}`
+    `🚀 Application is running on: ${`http://localhost:${port}`}/${globalPrefix}`
   );
 }
 bootstrap();
